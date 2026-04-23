e922d9e419692765bbe64bc4d5f834ec1cf0cdaa
andrewt33 <amtarnoski@gmail.com>
2026-04-23 11:49:39 -0400

    search updates

--- routes/location.js
+++ routes/location.js
@@ -1,50 +1,70 @@
 import { Router } from "express";
 import helpers from "../helpers.js";
 import * as restaurants from "../data/restaurants.js";
-import {loginGuard} from "../middleware.js";
+import { loginGuard } from "../middleware.js";
+import { CursorTimeoutMode } from "mongodb";
 
 const router = Router();
 
-/* // Changed this to be /home instead of /
-router.route("/").get(async (req, res) => {
-  return res.render("home", { title: "Location Search" });
-});
-*/
-
 router.route("/home").get(loginGuard, async (req, res) => {
-  return res.render("home", {title: "Location Search"});
+  return res.render("home", { title: "Restaurant Search" });
 });
 
 // Changed the default page when not signed in, into a advertising landing page for Delicacy
 router.route("/").get(async (req, res) => {
-  if (req.session.user){
+  if (req.session.user) {
     return res.redirect("/home"); // if logged in, go straight home
   }
-  return res.render("landing", {title: "Delicacy Restaurant Review App"} );
+  return res.render("landing", { title: "Delicacy Restaurant Review App" });
 });
 
-
 router.route("/searchResults").post(loginGuard, async (req, res) => {
-  //code here for POST this is where your form will be submitting keyword
-  //and then call your data function passing in the keyword and then
-  // rendering the search results of matching meals.
-  let info = req.body.location;
+  let info = req.body;
+  let name;
+  let boro;
+  let cuisine;
 
-  try {
-    info = helpers.checkString(info, "location");
-  } catch (e) {
-    let message = "You must enter a search term!";
-    return res
-      .status(400)
-      .render("error", { errorClass: "error", error: message });
+  if (info.name) {
+    name = info.name.trim();
+  } else {
+    name = "";
+  }
+
+  if (info.boro === "select") {
+    boro = "";
+  } else {
+    try {
+      boro = helpers.checkBoro(info.boro);
+    } catch (e) {
+      return res.status(400).render("error", {
+        errorClass: "error",
+        error: "Invalid Borough Location!",
+      });
+    }
+  }
+
+  if (info.cuisine) {
+    cuisine = info.cuisine.trim();
+  } else {
+    cuisine = "";
+  }
+
+  if (name === "" && boro === "" && cuisine === "") {
+    return res.status(400).render("error", {
+      errorClass: "error",
+      error: "You must supply a search term!",
+    });
   }
 
   try {
-    const data = await restaurants.search(info);
+    const data = await restaurants.search(name, boro, cuisine);
+    console.log(data);
     return res.render("searchResults", {
       title: "Restaurants Found",
-      keywords: info,
-      restaurantsData: data.restaurants,
+      name: name,
+      boro: boro,
+      cuisine: cuisine,
+      restaurantsData: data,
     });
   } catch (e) {
     return res
